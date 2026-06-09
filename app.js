958c8944ba14e4ac303f6f07beb3898c64ce6198
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-06-09 23:33:16 +0530

    test webhook sync event

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const API_KEY = "sk_live_123456789";
 function connect() {
   return API_KEY;
 }
+
+const API_KEY = "sk_live_123456789";
+const PASSWORD = "123456";
+const SECRET = "my-secret-token";
